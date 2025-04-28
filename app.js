d13919c3a0704cfa15a3ff621c0ea00016ba6863
ruloDev <rulo_dev@outlook.com>
2025-04-27 23:40:17 -0600

    agregando funcion de la barra de movimiento

--- app.js
+++ app.js
@@ -1,100 +1,117 @@
-const tituloCancion = document.querySelector('.reproductor-musica h1');
-const nombreArtista = document.querySelector('.reproductor-musica p');
-const portada = document.querySelector('#portada img');
+const tituloCancion = document.querySelector(".reproductor-musica h1");
+const nombreArtista = document.querySelector(".reproductor-musica p");
+const portada = document.querySelector("#portada img");
 
-const progreso = document.getElementById('progreso');
-const cancion = document.getElementById('cancion');
+const progreso = document.getElementById("progreso");
+const cancion = document.getElementById("cancion");
 
-const iconoControl = document.getElementById('iconoControl');
+const iconoControl = document.getElementById("iconoControl");
 const btnPlayPause = document.querySelector(
-  '.controles button.btn-reproducir-pausar'
+  ".controles button.btn-reproducir-pausar"
 );
 
-const botonAtras = document.querySelector('.controles button.atras');
-const botonSiguiente = document.querySelector('.controles button.siguiente');
+const botonAtras = document.querySelector(".controles button.atras");
+const botonSiguiente = document.querySelector(".controles button.siguiente");
 
 const canciones = [
   {
-    titulo: 'Take on Me',
-    nombre: 'a-ha',
-    fuente: 'music/a-ha - Take On Me (Video Oficial) __ Sub Español   Lyrics(MP3_320K).mp3',
+    titulo: "Take on Me",
+    nombre: "a-ha",
+    fuente:
+      "music/a-ha - Take On Me (Video Oficial) __ Sub Español   Lyrics(MP3_320K).mp3",
   },
   {
-    titulo: 'Skyfall',
-    nombre: 'Adele',
-    fuente: 'music/Adele - Skyfall (Official Lyric Video)(MP3_320K).mp3',
+    titulo: "Skyfall",
+    nombre: "Adele",
+    fuente: "music/Adele - Skyfall (Official Lyric Video)(MP3_320K).mp3",
   },
   {
-    titulo: 'Queen of Kings',
-    nombre: 'Alessandra',
-    fuente: 'music/Alessandra - Queen of Kings (Official Music Video)(MP3_320K).mp3'
+    titulo: "Queen of Kings",
+    nombre: "Alessandra",
+    fuente:
+      "music/Alessandra - Queen of Kings (Official Music Video)(MP3_320K).mp3",
   },
   {
-    titulo: 'You  Make Me (Remake)',
-    nombre: 'ANYMA _ RÜFÜS DU SOL',
-    fuente: 'music/ANYMA _ RÜFÜS DU SOL - You Make Me (Remake)(MP3_320K).mp3' 
+    titulo: "You  Make Me (Remake)",
+    nombre: "ANYMA _ RÜFÜS DU SOL",
+    fuente: "music/ANYMA _ RÜFÜS DU SOL - You Make Me (Remake)(MP3_320K).mp3",
   },
   {
-    titulo: 'Take on Me',
-    nombre: 'a-ha',
-    fuente: 'music/a-ha - Take On Me.mp3'
+    titulo: "Take on Me",
+    nombre: "a-ha",
+    fuente: "music/a-ha - Take On Me.mp3",
   },
   {
-    titulo: 'Ojitos Lindos',
-    nombre: 'Bad Bunny ft Bomba Estéreo',
-    fuente: 'music/Bad Bunny (ft. Bomba Estéreo) - Ojitos Lindos (360° Visualizer) _ Un Verano Sin Ti(MP3_320K).mp3'
+    titulo: "Ojitos Lindos",
+    nombre: "Bad Bunny ft Bomba Estéreo",
+    fuente:
+      "music/Bad Bunny (ft. Bomba Estéreo) - Ojitos Lindos (360° Visualizer) _ Un Verano Sin Ti(MP3_320K).mp3",
   },
   {
-    titulo: 'Take on Me',
-    nombre: 'a-ha',
-    fuente: 'music/a-ha - Take On Me.mp3'
+    titulo: "Take on Me",
+    nombre: "a-ha",
+    fuente: "music/a-ha - Take On Me.mp3",
   },
   {
-    titulo: 'Game of Thrones',
-    nombre: 'KZHRM',
-    fuente: 'music/Game Of Thrones (8D Audio)--(MP3_320K).mp3'
+    titulo: "Game of Thrones",
+    nombre: "KZHRM",
+    fuente: "music/Game Of Thrones (8D Audio)--(MP3_320K).mp3",
   },
   {
-    titulo: 'Injuria 2',
-    nombre: 'Uknown',
-    fuente: 'music/inuria2(MP3_320K).mp3'
+    titulo: "Injuria 2",
+    nombre: "Uknown",
+    fuente: "music/inuria2(MP3_320K).mp3",
   },
   {
-    titulo: 'Zero',
-    nombre: 'Insite',
-    fuente: 'music/Insite - Zero(MP3_320K).mp3'
-  }
-]
+    titulo: "Zero",
+    nombre: "Insite",
+    fuente: "music/Insite - Zero(MP3_320K).mp3",
+  },
+];
 
 let indiceCancionActual = 0;
 
-function actualizarInfoCancion(){
+function actualizarInfoCancion() {
   tituloCancion.textContent = canciones[indiceCancionActual].titulo;
   nombreArtista.textContent = canciones[indiceCancionActual].nombre;
   cancion.src = canciones[indiceCancionActual].fuente;
-  cancion.addEventListener('loadeddata', function(){});
+  cancion.addEventListener("loadeddata", function () {});
 }
 
-btnPlayPause.addEventListener('click', reproducirPausar);
+btnPlayPause.addEventListener("click", reproducirPausar);
 
 function reproducirPausar() {
-  if(cancion.paused){
+  if (cancion.paused) {
     reproducirCancion();
-    iconoControl.classList.add('bi-pause');
-    iconoControl.classList.remove('bi-play');
-  } else{
+  } else {
     pausarCancion();
-    iconoControl.classList.add('bi-play');
-    iconoControl.classList.remove('bi-pause');
   }
-  
-};
+}
 
-function reproducirCancion(){
+function reproducirCancion() {
   cancion.play();
+  iconoControl.classList.add("bi-pause");
+  iconoControl.classList.remove("bi-play");
 }
-function pausarCancion(){
+function pausarCancion() {
   cancion.pause();
+  iconoControl.classList.add("bi-play");
+  iconoControl.classList.remove("bi-pause");
 }
+
+//la barra de progreso se mueve automaticamente cuando la cancion avanza
+cancion.addEventListener("timeupdate", () => {
+  if (!cancion.paused) {
+    progreso.value = cancion.currentTime;
+  }
+});
+//esta funcion es para que muevas la barra de progreso y se reproduzca la cancion desde el tiempo que le des
+progreso.addEventListener("input", () => {
+  cancion.currentTime = progreso.value;
+});
+//esta funcion es para que la cancion inicie cuando la barra de progreso se mueve
+progreso.addEventListener("change", () => {
+  reproducirCancion();
+});
+
 actualizarInfoCancion();
-// reproducirPausar();
